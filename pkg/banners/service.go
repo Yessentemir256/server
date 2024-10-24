940ac37e73b72fd8d632e9e0abee2b5c245ef576
Yessentemir <yessentemir.utepov@gmail.com>
2024-10-24 23:45:29 +0500

    service.go

---
+++ pkg/banners/service.go
@@ -0,0 +1,46 @@
+package banners
+
+import (
+	"context"
+	"sync"
+)
+
+// Service представляет собой сервис по управлению баннерами.
+type Service struct {
+	mu    sync.RWMutex
+	items []*Banner
+}
+
+// NewService создаёт сервис.
+func NewService() *Service {
+	return &Service{items: make([]*Banner, 0)}
+}
+
+// Banner представляет собой баннер.
+type Banner struct {
+	ID      int64
+	Title   string
+	Content string
+	Button  string
+	Link    string
+}
+
+// All возвращает все существующие баннеры.
+func (s *Service) All(ctx context.Context) ([]*Banner, error) {
+	panic("not implemented")
+}
+
+// ByID возвращает баннер по идентификатору.
+func (s *Service) ByID(ctx context.Context, id int64) (*Banner, error) {
+	panic("not implemented")
+}
+
+// Save сохраняет/обновляет баннер.
+func (s *Service) Save(ctx context.Context, item *Banner) (*Banner, error) {
+	panic("not implemented")
+}
+
+// RemoveByID удаляет баннер по идентификатору.
+func (s *Service) RemoveByID(ctx context.Context, id int64) (*Banner, error) {
+	panic("not implemented")
+}
